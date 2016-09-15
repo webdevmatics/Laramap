1df0634410760d6fbd1d07deb46c4e621c4b7288
Subhendu Bhatta <bhattasubhendu@gmail.com>
2016-09-15 20:07:22 +0545

    Finding current location of user

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,39 @@
+var map;
+var myLatLng;
 $(document).ready(function() {
-    var myLatLng = new google.maps.LatLng(-33.8665433, 151.1956316);
+    geoLocationInit();
 
+    function geoLocationInit() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, fail);
+        } else {
+            alert("Browser not supported");
+        }
+    }
+
+    function success(position) {
+        console.log(position);
+        var latval = position.coords.latitude;
+        var lngval = position.coords.longitude;
+        myLatLng = new google.maps.LatLng(latval, lngval);
+        createMap(myLatLng);
+        nearbySearch(myLatLng, "school");
+    }
+
+    function fail() {
+        alert("it fails");
+    }
     //Create Map
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 12
-    });
+    function createMap(myLatLng) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 12
+        });
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map
+        });
+    }
     //Create marker
     function createMarker(latlng, icn, name) {
         var marker = new google.maps.Marker({
@@ -15,25 +43,26 @@ $(document).ready(function() {
             title: name
         });
     }
-
     //Nearby search
-    var request = {
-        location: myLatLng,
-        radius: '2500',
-        types: ['school']
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+    function nearbySearch(myLatLng, type) {
+        var request = {
+            location: myLatLng,
+            radius: '2500',
+            types: [type]
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
 
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var place = results[i];
-                console.log(place);
-                latlng = place.geometry.location;
-                icn = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-                name = place.name;
-                createMarker(latlng, icn, name);
+        function callback(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    var place = results[i];
+                    console.log(place);
+                    latlng = place.geometry.location;
+                    icn = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+                    name = place.name;
+                    createMarker(latlng, icn, name);
+                }
             }
         }
     }
